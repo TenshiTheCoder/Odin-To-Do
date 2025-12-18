8d5d8da87237f966964a5e927088002a0274c7be
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2025-12-18 18:10:55 -0500

    Add header and footer elements to themeButton and include in applyTheme

--- src/themeButton.js
+++ src/themeButton.js
@@ -3,6 +3,7 @@ import {Project} from "./barrel.js";
 export function themeButton() {
   const headerContainer = document.querySelector("#main-header");
   const bodyContainer = document.querySelector("#body");
+  const footerContainer = document.querySelector("#footer");
 
   const themeSelect = document.createElement("select");
   themeSelect.name = "themeSelect";
@@ -27,9 +28,17 @@ export function themeButton() {
     if(themeSelect.value === "Light") {
       bodyContainer.classList.add("light-theme");
       bodyContainer.classList.remove("dark-theme");
+      headerContainer.classList.add("light-theme");
+      headerContainer.classList.remove("dark-theme");
+      footerContainer.classList.add("light-theme");
+      footerContainer.classList.remove("dark-theme");
     } else {
       bodyContainer.classList.add("dark-theme");
       bodyContainer.classList.remove("light-theme");
+      headerContainer.classList.add("dark-theme");
+      headerContainer.classList.remove("light-theme");
+      footerContainer.classList.add("dark-theme");
+      footerContainer.classList.remove("light-theme");
     }
   }
 
